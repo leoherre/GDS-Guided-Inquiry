897fc2965c793fc0aca00b2d8a04ea0f7f7df749
leoherrera <leoherre@usc.edu>
2022-07-07 18:02:13 -0400

    Finished all the levels of diffculty

---
+++ guidedInquiry.js
@@ -0,0 +1,43 @@
+//VERY EASY
+// var num1 = prompt("Enter a number ");
+// var num2 = prompt("Enter another number ");
+
+// function getMin(num1, num2){
+//     console.log(Math.min(num1, num2)) 
+// };
+// var minNumber = getMin(num1, num2);
+
+//EASY
+//  Students = [["John" ,"Doe", 19],["Mary", "Jane", 21],["Leo", "Herrera", 19]];
+//  console.log(`Hello my name is ${Students[1][0]} ${Students[1][1]} and I am ${Students[1][2]} years old.`);
+
+ //MEDIUM
+// let numMonth = prompt("Please enter a number 1-12");
+// console.log(numMonth);
+
+// if(numMonth < 1 || numMonth > 12){
+//     console.log("Number is out of bounds");
+// }
+// Months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// for(let i = 0; i < Months.length;i++){
+//     if((i+1) == numMonth){
+//         console.log(Months[i]);
+        
+//     }
+// }
+
+//HARD
+
+// var tomHeight = 9;
+// var tomMass = 8;
+// var jerryHieght = 10;
+// var jerryMass = 45;
+
+// // BMI = mass / (height * height)
+// var tomBMI = tomMass/ (tomHeight*tomHeight);
+// var jerryBMI = jerryMass/ (jerryHieght*jerryHieght);
+// console.log(tomBMI);
+// console.log(jerryBMI);
+// //greaterTom is holding a boolean value
+// var greaterTom = (tomBMI > jerryBMI);
+// console.log(`Is Tom’s BMI higher than Jerry’s? ${greaterTom}`);
